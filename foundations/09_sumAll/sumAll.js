f3bac122b4ccfe7782096bdfa809a4aa067274a7
mohamed eldaly <21154454+mohamedeldaly@users.noreply.github.com>
2026-05-16 01:04:44 +0300

    finish task

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (num1, num2) {
+  let result = 0;
+  if (num1 < num2) {
+    for (let i = num1; i <= num2; num1++) {
+      result += num1;
+    }
+  } else {
+    for (let i = num2; i <= num1; num2++) {
+      result += num2;
+    }
+  }
+  return result;
 };
 
 // Do not edit below this line
