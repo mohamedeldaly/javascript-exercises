bb3c3780c4630516f987b4a65c42ea8e89701e20
mohamed eldaly <21154454+mohamedeldaly@users.noreply.github.com>
2026-05-16 01:16:22 +0300

    fix infinite loop and the missing test requriments

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,12 +1,20 @@
 const sumAll = function (num1, num2) {
+  if (
+    num1 < 0 ||
+    num2 < 0 ||
+    !Number.isInteger(num1) ||
+    !Number.isInteger(num2)
+  ) {
+    return "ERROR";
+  }
   let result = 0;
   if (num1 < num2) {
-    for (let i = num1; i <= num2; num1++) {
-      result += num1;
+    for (let i = num1; i <= num2; i++) {
+      result += i;
     }
   } else {
-    for (let i = num2; i <= num1; num2++) {
-      result += num2;
+    for (let i = num2; i <= num1; i++) {
+      result += i;
     }
   }
   return result;
