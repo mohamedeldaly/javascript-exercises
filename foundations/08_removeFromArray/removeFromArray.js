c70f10c63d8804f597bea702a60f769bb962326e
mohamed eldaly <21154454+mohamedeldaly@users.noreply.github.com>
2026-05-16 00:58:44 +0300

    finish task

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+  for (let i = 0; i < items.length; i++) {
+    let index = arr.indexOf(items[i]);
+    if (index !== -1) {
+      arr.splice(index, 1);
+      i--;
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
